7849b76a9ae8e638ed11c03d7f043224abc4706c
Maryam Hashemi <maryam.hashemy66@gmail.com>
2022-12-11 18:46:12 +0330

    Add files via upload

--- Parking.kt
+++ Parking.kt
@@ -2,9 +2,8 @@ package com.example.trainingforinterview.talpa
 
 fun main() {
     val parking = Parking(10, 5)
-    parking.parkVehicle(VehicleType.TRUCK)
-    parking.unPark(VehicleType.TRUCK)
 }
+
 class Parking(var freeCarSpot: Int, var freeTruckSpot: Int) {
 
     private var carSpotList = mutableListOf<Vehicle>()
@@ -12,33 +11,40 @@ class Parking(var freeCarSpot: Int, var freeTruckSpot: Int) {
     private var bikeSpotList = mutableListOf<Vehicle>()
     private var freeBikeSpot: Int = 0
 
-    fun parkVehicle(vehicle: VehicleType) {
-        if (vehicle == VehicleType.TRUCK) {
-            if (freeTruckSpot > 0) {
-                parkYourTruck()
-            } else {
-                println("there is no enough space for your truck")
-            }
-        } else if (vehicle == VehicleType.CAR) {
-            if (freeCarSpot > 0) {
-                parkYourCar()
-            } else if (freeTruckSpot > 0) {
-                parkYourCarInTruckSpot()
-            } else {
-                println("there is no enough space for your car")
-            }
-        } else if (vehicle == VehicleType.BIKE) {
-            if (freeBikeSpot in 1..2) {
-                parkYourBike()
-            } else if ((freeTruckSpot * 3) > 0) {
-                parkYourBike()
-            } else {
-                println("there is no enough space for your bike")
-            }
-        }
-//        println("$freeCarSpot $freeTruckSpot $freeBikeSpot")
-
+    fun String.tooooo(): String {
+        return ""
     }
+
+    fun VehicleType.TRUCK.parkVehicle() {
+        if (freeTruckSpot > 0) {
+            parkYourTruck()
+        } else {
+            println("there is no enough space for your truck")
+        }
+    }
+
+    fun VehicleType.CAR.parkVehicle() {
+        if (freeCarSpot > 0) {
+            parkYourCar()
+        } else if (freeTruckSpot > 0) {
+            parkYourCarInTruckSpot()
+        } else {
+            println("there is no enough space for your car")
+        }
+    }
+
+    fun VehicleType.BIKE.parkVehicle() {
+        if (freeBikeSpot in 1..2) {
+            parkYourBike()
+        } else if ((freeTruckSpot * 3) > 0) {
+            parkYourBike()
+        } else {
+            println("there is no enough space for your bike")
+        }
+    }
+
+    //        println("$freeCarSpot $freeTruckSpot $freeBikeSpot")
+
 
     private fun parkYourCarInTruckSpot() {
         carSpotList.add(Vehicle(VehicleType.CAR, Destination.TRUCK))
@@ -72,41 +78,45 @@ class Parking(var freeCarSpot: Int, var freeTruckSpot: Int) {
             freeCarSpot -= 2
     }
 
-    fun unPark(vehicle: VehicleType) {
-        if (vehicle == VehicleType.TRUCK) {
-            if (truckSpotList.size > 0) {
-                println("successfully unpark")
-                freeTruckSpot++
-                truckSpotList.removeLast()
-            } else {
-                println("there is no truck here")
-            }
-        } else if (vehicle == VehicleType.CAR) {
-            if (carSpotList.size > 0) {
-                println("successfully unpark")
-                if (carSpotList.last().destinationType == Destination.TRUCK) {
-                    freeTruckSpot++
-                    freeCarSpot--
-                    carSpotList.removeLast()
-                } else {
-                    freeCarSpot++
-                    carSpotList.removeLast()
-                }
-            } else {
-                println("there is no car here")
-            }
-        } else if (vehicle == VehicleType.BIKE) {
-            if (bikeSpotList.size > 0) {
-                println("successfully unpark")
-                freeBikeSpot++
-                bikeSpotList.removeLast()
-                increaseTruckSpotByBike(freeBikeSpot)
-            } else {
-                println("there is no bike here")
-            }
+    fun VehicleType.TRUCK.unParkVehicle() {
+        if (truckSpotList.size > 0) {
+            println("successfully unpark")
+            freeTruckSpot++
+            truckSpotList.removeLast()
+        } else {
+            println("there is no truck here")
         }
-//        println("$freeCarSpot $freeTruckSpot $freeBikeSpot")
     }
+
+    fun VehicleType.CAR.unParkVehicle() {
+        if (carSpotList.size > 0) {
+            println("successfully unpark")
+            if (carSpotList.last().destinationType == Destination.TRUCK) {
+                freeTruckSpot++
+                freeCarSpot--
+                carSpotList.removeLast()
+            } else {
+                freeCarSpot++
+                carSpotList.removeLast()
+            }
+        } else {
+            println("there is no car here")
+        }
+    }
+
+    fun VehicleType.BIKE.unParkVehicle() {
+        if (bikeSpotList.size > 0) {
+            println("successfully unpark")
+            freeBikeSpot++
+            bikeSpotList.removeLast()
+            increaseTruckSpotByBike(freeBikeSpot)
+        } else {
+            println("there is no bike here")
+        }
+    }
+
+    //        println("$freeCarSpot $freeTruckSpot $freeBikeSpot")
+
 
     private fun increaseTruckSpotByBike(numberOfFreeBikeSpot: Int) {
         if (numberOfFreeBikeSpot != 0 && numberOfFreeBikeSpot % 3 == 0) {
@@ -114,21 +124,22 @@ class Parking(var freeCarSpot: Int, var freeTruckSpot: Int) {
             freeBikeSpot -= 3
         }
     }
+
+    sealed class VehicleType {
+        object CAR : VehicleType()
+        object BIKE : VehicleType()
+        object TRUCK : VehicleType()
+
+    }
+
+    data class Vehicle(
+        val vehicleType: VehicleType,
+        val destinationType: Destination
+    )
+
+    sealed class Destination {
+        object CAR : Destination()
+        object TRUCK : Destination()
+    }
 }
 
-sealed class VehicleType {
-    object CAR : VehicleType()
-    object BIKE : VehicleType()
-    object TRUCK : VehicleType()
-
-}
-
-data class Vehicle(
-    val vehicleType: VehicleType,
-    val destinationType: Destination
-)
-
-sealed class Destination {
-    object CAR : Destination()
-    object TRUCK : Destination()
-}
